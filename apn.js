ce9c82068951169633e067d1ceff6896dba2201d
aston314 <aston.he@gmail.com>
2024-07-26 12:11:06 +0800

    Update apn.js

--- apn.js
+++ apn.js
@@ -2,6 +2,9 @@
 const TMDB_API_BASE = "https://api.themoviedb.org/3/";
 const TMDB_IMAGE_BASE = "https://image.tmdb.org/t/p/";
 const EZTV_API_BASE = "https://eztvx.to/";
+const YTS_API_BASE = "https://yts.mx/api/v2/";
+const TB_API_BASE = "https://thepiratebay10.info/search/";
+const X_API_BASE = "https://1337x.to/sort-category-search/";
 
 async function proxyRequest(url: string, request: Request): Promise<Response> {
   const response = await fetch(url, {
@@ -28,6 +31,15 @@ async function handleRequest(request: Request): Promise<Response> {
   } else if (path.startsWith("/api/get-torrents")) {
     // EZTV API 代理
     return proxyRequest(EZTV_API_BASE + path + url.search, request);
+  } else if (path.startsWith("/api/v2/")) {
+    // YTS API 代理
+    return proxyRequest(YTS_API_BASE + path.slice(8) + url.search, request);
+  } else if (path.startsWith("/search/")) {
+    // TB API 代理
+    return proxyRequest(TB_API_BASE + path.slice(8) + url.search, request);
+  } else if (path.startsWith("/sort-category-search/")) {
+    // X API 代理
+    return proxyRequest(X_API_BASE + path.slice(22) + url.search, request);
   } else {
     return new Response("Not Found", { status: 404 });
   }
